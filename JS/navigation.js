9bdbdcff8e957ab59643284905a2747ce4622dd7
Oliver Christian Koldkjær Jørgensen <339603@RTS-365.dk>
2019-02-14 13:42:27 +0100

    Update navigation.js

--- JS/navigation.js
+++ JS/navigation.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let nav = document.querySelector(".sideNav");
     let sideNavBar = document.querySelector(".sideNav__bar");
-
+ 
     nav.addEventListener("click", (event) => {
         if (event.target.className == "sideNav__open") {
             sideNavBar.style.width = "250px";
